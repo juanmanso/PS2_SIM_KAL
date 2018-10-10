19cc6d9d72371f61d79d508259ec416e7da9d58d
MatiasAnas <matias.anas@gmail.com>
2018-10-10 18:06:12 -0300

    Adapte EJ3 a matlab.
    Adapte Ej3 a matlab.

--- Octave/EJ3.m
+++ Octave/EJ3.m
@@ -1,6 +1,6 @@
 config_m;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% EJ KALMAN - EstimaciÃ³n con distintos valores iniciales
+% EJ KALMAN - Estimación con distintos valores iniciales
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 datos_str = load('datos.mat');
@@ -10,8 +10,8 @@ Tiempo = datos_str.tiempo;
 Pos = datos_str.Pos;
 Vel = datos_str.Vel;
 
-dim = 2;			% Se considera sÃ³lo x e y
-tipos_variables = 3;		% PosiciÃ³n, Velocidad, AceleraciÃ³n
+dim = 2;			% Se considera sólo x e y
+tipos_variables = 3;		% Posición, Velocidad, Aceleración
 cant_mediciones = length(Pos);
 cant_estados = tipos_variables * dim;
 
@@ -77,7 +77,7 @@ M_eta = [randn(dim,cant_mediciones)*sigma_etap*bool_p;
        	randn(dim,cant_mediciones)*sigma_etaa*bool_a];
 
 yk = C * [Pos(:,1:dim) Vel(:,1:dim) Acel(:,1:dim)]' + (C*M_eta);
-yk = yk'; % AsÃ­ tiene la forma de Pos
+yk = yk'; % Así tiene la forma de Pos
 
 R = diag([ones(1,dim*bool_p)*sigma_etap^2 ones(1,dim*bool_v)*sigma_etav^2 ones(1,dim*bool_a)*sigma_etaa^2]);
 
@@ -90,17 +90,17 @@ Pk1_k1 = P;
 g = yk(1,:)';
 
 for i=1:cant_mediciones-1
-	% PredicciÃ³n
+	% Predicción
 	xk_k1 = Ad * xk1_k1;
 	Pk_k1 =	Ad * Pk1_k1 * Ad' + Bk1 * Qd * Bk1';
 	gk = [innovaciones(yk(i,:),C,xk_k1)];
 
-	% CorrecciÃ³n
+	% Corrección
 	Kk = Pk_k1 * C'*(R + C*Pk_k1*C')^-1;
 	xk_k = xk_k1 + Kk*(gk);
 	Pk_k = (eye(cant_estados) - Kk*C) * Pk_k1;
 	
-	% ActualizaciÃ³n
+	% Actualización
 	xk1_k1 = xk_k;
 	Pk1_k1 = Pk_k;
 
@@ -135,22 +135,22 @@ for l = 1:4
 	Pk1_k1 = Pa;
 
 	for i = 1:cant_mediciones-1
-		% PredicciÃ³n
+		% Predicción
 		xk_k1 = Ad * xk1_k1;
 		Pk_k1 =	Ad * Pk1_k1 * Ad' + Bk1 * Qd * Bk1';
 	
-		% CorrecciÃ³n
+		% Corrección
 		Kk = Pk_k1 * C'*(R + C*Pk_k1*C')^-1;
 		xk_k = xk_k1 + Kk*(yk(i,:)' - C*xk_k1);
 		Pk_k = (eye(dim*3) - Kk*C) * Pk_k1;
 		
 	% PARA HACER SIMETRICA P, ALEJANDOSE DEL VALOR VERDADERO	
-	%		% Para hacerlo simÃ©trico
+	%		% Para hacerlo simétrico
 	%		for i=1:2
 	%			Pk_k=(Pk_k+Pk_k')/2;
 	%		end
 	
-		% ActualizaciÃ³n
+		% Actualización
 		xk1_k1 = xk_k;
 		Pk1_k1 = Pk_k;
 	
@@ -184,11 +184,14 @@ plot(x(1,:),x(2,:),'LineWidth',3)
 %	plot(x4(1,:),x4(2,:),'-','LineWidth',3,'color',[0.8 0.2 1])
 plot(Pos(:,1),Pos(:,2),'r','LineWidth',2)
 plot(yk(:,1),yk(:,2),'color',myGreen)
-title('EstimaciÃ³n');
-legend(['Estimada'; 'Medida'; 'Ruidosa']);
-legend('location','NorthWest')
-xlabel=('PosiciÃ³n $x$ [\si{\m}]');
-ylabel=('PosiciÃ³n $y$ [\si{\m}]');
+title('Estimación');
+if(EsMatlab == 1)
+    legend('Estimada','Medida','Ruidosa','location','SouthEast');
+else
+    legend(['Estimada';'Medida';'Ruidosa'],'location','SouthEast');
+end
+xlabel=('Posición $x$ [\si{\m}]');
+ylabel=('Posición $y$ [\si{\m}]');
 
 
 % Grafico de medida, estimada, ruidosa
@@ -198,11 +201,14 @@ grid
 plot(x1(1,:),x1(2,:),'LineWidth',3)
 plot(Pos(:,1),Pos(:,2),'r','LineWidth',2)
 plot(yk(:,1),yk(:,2),'color',myGreen)
-title('EstimaciÃ³n con x1');
-legend(['Estimada';'Medida';'Ruidosa']);
-legend('location','NorthWest')
-xlabel=('PosiciÃ³n $x$ [\si{\m}]');
-ylabel=('PosiciÃ³n $y$ [\si{\m}]');
+title('Estimación');
+if(EsMatlab == 1)
+    legend('Estimada','Medida','Ruidosa','location','SouthEast');
+else
+    legend(['Estimada';'Medida';'Ruidosa'],'location','SouthEast');
+end
+xlabel=('Posición $x$ [\si{\m}]');
+ylabel=('Posición $y$ [\si{\m}]');
 
 
 % Grafico de medida, estimada, ruidosa
@@ -212,11 +218,14 @@ grid
 plot(x2(1,:),x2(2,:),'LineWidth',3)
 plot(Pos(:,1),Pos(:,2),'r','LineWidth',2)
 plot(yk(:,1),yk(:,2),'color',myGreen)
-title('EstimaciÃ³n con x2');
-legend(['Estimada';'Medida';'Ruidosa']);
-legend('location','NorthWest')
-xlabel=('PosiciÃ³n $x$ [\si{\m}]');
-ylabel=('PosiciÃ³n $y$ [\si{\m}]');
+title('Estimación');
+if(EsMatlab == 1)
+    legend('Estimada','Medida','Ruidosa','location','SouthEast');
+else
+    legend(['Estimada';'Medida';'Ruidosa'],'location','SouthEast');
+end
+xlabel=('Posición $x$ [\si{\m}]');
+ylabel=('Posición $y$ [\si{\m}]');
 
 % Grafico de medida, estimada, ruidosa
 figure
@@ -225,11 +234,14 @@ grid
 plot(x3(1,:),x3(2,:),'LineWidth',3)
 plot(Pos(:,1),Pos(:,2),'r','LineWidth',2)
 plot(yk(:,1),yk(:,2),'color',myGreen)
-title('EstimaciÃ³n con x3');
-legend('location','NorthWest')
-legend(['Estimada';'Medida';'Ruidosa']);
-xlabel=('PosiciÃ³n $x$ [\si{\m}]');
-ylabel=('PosiciÃ³n $y$ [\si{\m}]');
+title('Estimación');
+if(EsMatlab == 1)
+    legend('Estimada','Medida','Ruidosa','location','SouthEast');
+else
+    legend(['Estimada';'Medida';'Ruidosa'],'location','SouthEast');
+end
+xlabel=('Posición $x$ [\si{\m}]');
+ylabel=('Posición $y$ [\si{\m}]');
 
 % Grafico de medida, estimada, ruidosa
 figure
@@ -238,11 +250,14 @@ grid
 plot(x4(1,:),x4(2,:),'LineWidth',3)
 plot(Pos(:,1),Pos(:,2),'r','LineWidth',2)
 plot(yk(:,1),yk(:,2),'color',myGreen)
-title('EstimaciÃ³n con x4');
-legend(['Estimada';'Medida';'Ruidosa']);
-legend('location','NorthWest')
-xlabel=('PosiciÃ³n $x$ [\si{\m}]');
-ylabel=('PosiciÃ³n $y$ [\si{\m}]');
+title('Estimación');
+if(EsMatlab == 1)
+    legend('Estimada','Medida','Ruidosa','location','SouthEast');
+else
+    legend(['Estimada';'Medida';'Ruidosa'],'location','SouthEast');
+end
+xlabel=('Posición $x$ [\si{\m}]');
+ylabel=('Posición $y$ [\si{\m}]');
 
 
 %%%%%%%%%% CÃ³mo grafico el error?!?! %%%%%%%%%%%%%%%%%%
